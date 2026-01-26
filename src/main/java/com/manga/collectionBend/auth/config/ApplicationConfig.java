5a1b1a5a667848fb9657397ed99c073221245727
vrevanthsai <rinkurevanth2@gmail.com>
2026-01-26 13:50:04 +0530

    removed deprecated AuthenticationProvider method usage and only AuthenticationManager method is enough

--- src/main/java/com/manga/collectionBend/auth/config/ApplicationConfig.java
+++ src/main/java/com/manga/collectionBend/auth/config/ApplicationConfig.java
@@ -25,25 +25,27 @@ public class ApplicationConfig {
     @Bean // creates object for spring security
     //  UserDetailsService - which loads user-specific data and used as user DAO/Object
     public UserDetailsService userDetailsService(){
-//  UserDetailsService -is functional Interface - so we can use Lambda expression to gets its object
+//  UserDetailsService -is functional Interface - so we can use Lambda expression to get its object
 //  we are providing its implementation in the form of lambda expressions
 //        here we use email for username
-        return username -> userRepo.findByUsername(username)
+        return username -> userRepo.findByEmail(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + username));
     }
 
-    @Bean
-//    this method used for authenticating the user
-//    indicates a class can process a specific Authentication implementation
-    public AuthenticationProvider authenticationProvider(){
-//        as UserDeatilsService uses DaoAuthentication- we create a object for it
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-//        this will set all userDetails with provider
-        authenticationProvider.setUserDetailsService(userDetailsService());
-//        this will encode password of user before storing instead of raw data
-        authenticationProvider.setPasswordEncoder(passwordEncoder());
-        return authenticationProvider;
-    }
+//    -- contains Deprecated Methods for DaoAuthenticationProvider() and setPasswordEncoder()
+//    You actually don’t need DaoAuthenticationProvider at all in most cases. so use only AuthenticationManager() method only
+//    @Bean
+////    this method used for authenticating the user
+////    indicates a class can process a specific Authentication implementation
+//    public AuthenticationProvider authenticationProvider(){
+////        as UserDeatilsService uses DaoAuthentication- we create a object for it
+//        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+////        this will set all userDetails with provider
+//        authenticationProvider.setUserDetailsService(userDetailsService());
+////        this will encode password of user before storing instead of raw data
+//        authenticationProvider.setPasswordEncoder(passwordEncoder());
+//        return authenticationProvider;
+//    }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
@@ -52,6 +54,8 @@ public class ApplicationConfig {
     }
 
 //    we need a method for processing Authentication(username/pwd) Request
+//    You actually don’t need DaoAuthenticationProvider at all in most cases but Both UserDetailsService and PasswordEncoder methods must be available in ApplicationConfig file
+//    Spring Security auto-configures it when you expose UserDetailsService and PasswordEncoder.
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
