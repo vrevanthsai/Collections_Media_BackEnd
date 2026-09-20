60e0b7a665e7cc4304ff5f43c1e6104c10508520
vrevanthsai <rinkurevanth2@gmail.com>
2026-09-20 19:34:55 +0530

    added new Admin api methods to manage Default Categories

--- src/main/java/com/manga/collectionBend/controllers/AdminController.java
+++ src/main/java/com/manga/collectionBend/controllers/AdminController.java
@@ -1,6 +1,11 @@
 package com.manga.collectionBend.controllers;
 
+import com.manga.collectionBend.dto.ApiResponse;
+import com.manga.collectionBend.dto.CategoryDto;
+import com.manga.collectionBend.dto.DefaultCategoryDto;
 import com.manga.collectionBend.dto.UserDto;
+import com.manga.collectionBend.entities.DefaultCategoryEntity;
+import com.manga.collectionBend.repositories.DefaultCategoryRepo;
 import com.manga.collectionBend.service.AdminService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -17,9 +22,11 @@ import java.util.List;
 @RequestMapping("/api/v1/user/{userId}/admin")
 public class AdminController {
     private final AdminService adminService;
+    private final DefaultCategoryRepo defaultCategoryRepo;
 
-    public AdminController(AdminService adminService) {
+    public AdminController(AdminService adminService, DefaultCategoryRepo defaultCategoryRepo) {
         this.adminService = adminService;
+        this.defaultCategoryRepo = defaultCategoryRepo;
     }
 
     //    GET- All Users API
@@ -47,5 +54,56 @@ public class AdminController {
     @DeleteMapping("/delete-user/{userAccountId}")
     public ResponseEntity<String> deleteUser(@PathVariable Integer userAccountId) throws IOException {
         return ResponseEntity.ok(adminService.deleteUserHandler(userAccountId));
+    }
+
+//    Admin-based - Default Categories Apis
+    @GetMapping("/get-all-default-categories")
+    public ApiResponse<List<DefaultCategoryDto>> getAll() {
+        List<DefaultCategoryDto> all = defaultCategoryRepo.findAll().stream()
+            .map(DefaultCategoryDto::fromDefaultEntity)
+            .toList();
+        return ApiResponse.success(all);
+    }
+
+    @PostMapping("/create-default-category")
+    public ApiResponse<String> addDefaultCategory(@RequestParam String name) {
+        DefaultCategoryEntity entity = new DefaultCategoryEntity();
+        entity.setCategoryName(name);
+        entity.setActive(true);
+        defaultCategoryRepo.save(entity);
+        return ApiResponse.success("Default category added");
+    }
+
+    @PatchMapping("/update-default-category/{id}")
+    public ApiResponse<String> editDefaultCategory(@PathVariable Integer id, @RequestParam String newName) {
+        var entity = defaultCategoryRepo.findById(id)
+                .orElseThrow(() -> new RuntimeException("Default category not found with id: " + id));
+
+        // prevent renaming to a name that already exists on another default category
+        if (defaultCategoryRepo.existsByCategoryNameAndIdNot(newName, id)) {
+            return ApiResponse.error("A default category with this name already exists");
+        }
+
+        entity.setCategoryName(newName);
+        defaultCategoryRepo.save(entity);
+        return ApiResponse.success("Default category updated");
+    }
+
+//    instead of Deleting Default category value which will be linked to User based categories - we edit this deactivate  or activate value to false(meaning it will not be used anymore)
+//    MAJORLY - This 2 Api will not be used
+    @PatchMapping("/{id}/deactivate")
+    public ApiResponse<String> deactivate(@PathVariable Integer id) {
+        var entity = defaultCategoryRepo.findById(id).orElseThrow();
+        entity.setActive(false);
+        defaultCategoryRepo.save(entity);
+        return ApiResponse.success("Default category deactivated");
+    }
+    @PatchMapping("/{id}/activate")
+    public ApiResponse<String> activate(@PathVariable Integer id) {
+        var entity = defaultCategoryRepo.findById(id)
+                .orElseThrow(() -> new RuntimeException("Default category not found with id: " + id));
+        entity.setActive(true);
+        defaultCategoryRepo.save(entity);
+        return ApiResponse.success("Default category activated");
     }
 }
